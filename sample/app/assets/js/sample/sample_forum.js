eb2c7f2698360d6a1b28642eb0843f3679753e32
verdverm <verdverm@gmail.com>
2014-04-28 21:19:01 -0400

    fixing error in sample_files.js, auto-formatted some code

--- sample/app/assets/js/sample/sample_forum.js
+++ sample/app/assets/js/sample/sample_forum.js
@@ -1,15 +1,14 @@
 function initForumView() {
-    console.log("initForumView");
-	$("#forum-filter-button").on("click", handle_forum_filter_button_click);
-    $("#forum-reply-button").on("click", handle_forum_reply_button_click);
-    $("#forum-new-topic-button").on("click", handle_forum_newtopic_button_click);
-
+  console.log("initForumView");
+  $("#forum-filter-button").on("click", handle_forum_filter_button_click);
+  $("#forum-reply-button").on("click", handle_forum_reply_button_click);
+  $("#forum-new-topic-button").on("click", handle_forum_newtopic_button_click);
 }
 
 function handle_forum_filter_button_click(e) {
-	// dev debug printing
-    console.log("filter button was clicked");
-    console.log(e);
+  // dev debug printing
+  console.log("filter button was clicked");
+  console.log(e);
 
     // post filter values to server
     var id = $("#forum-filter-topicid").val();
@@ -26,16 +25,16 @@ function handle_forum_reply_button_click(e) {
     console.log("reply button was clicked");
     console.log(e);
 
-       var forum_newmessage_text = [
-    '    <div class="row">',
-    '       <div class="large-12 small-12 columns">',
-    '           <input type="hidden" name="csrf_token" value="{{ .csrf_token }}" />',
-    '           <a class="button" id="message_post_button">Post</a>',
-    '       </div>',
-    '    </div>',
-    '    <div class="row">',
-    '       <div id="epiceditor"></div>',
-    '    </div>',
+    var forum_newmessage_text = [
+        '    <div class="row">',
+        '       <div class="large-12 small-12 columns">',
+        '           <input type="hidden" name="csrf_token" value="{{ .csrf_token }}" />',
+        '           <a class="button" id="message_post_button">Post</a>',
+        '       </div>',
+        '    </div>',
+        '    <div class="row">',
+        '       <div id="epiceditor"></div>',
+        '    </div>',
     ].join("\n");
 
 
@@ -52,18 +51,18 @@ function handle_forum_newtopic_button_click(e) {
     console.log("new  topic button was clicked");
     console.log(e);
     var forum_newtopic_text = [
-    '    <div class="row">',
-    '       <div class="large-9 small-9 columns">',
-    '           <input type="hidden" name="csrf_token" value="{{ .csrf_token }}" />',
-    '           <input id="subject_field" type="text" placeholder="Subject">',
-    '       </div>',
-    '       <div class="large-3 small-3 columns">',
-    '           <a class="button" id="topic_post_button">Post</a>',
-    '       </div>',
-    '    </div>',
-    '    <div class="row">',
-    '       <div id="epiceditor"></div>',
-    '    </div>',
+        '    <div class="row">',
+        '       <div class="large-9 small-9 columns">',
+        '           <input type="hidden" name="csrf_token" value="{{ .csrf_token }}" />',
+        '           <input id="subject_field" type="text" placeholder="Subject">',
+        '       </div>',
+        '       <div class="large-3 small-3 columns">',
+        '           <a class="button" id="topic_post_button">Post</a>',
+        '       </div>',
+        '    </div>',
+        '    <div class="row">',
+        '       <div id="epiceditor"></div>',
+        '    </div>',
     ].join("\n");
 
 
@@ -146,7 +145,7 @@ function dosend_newmessage_post(content, topicId, csrf) {
             if (xhr.status === 200) {
                 // var results = JSON.parse(xhr.responseText);
                 console.log(xhr.responseText);
-                
+
             } else {
                 console.error(xhr.statusText);
             }
@@ -176,7 +175,7 @@ function dosend_newtopic_post(subject, content, csrf) {
             if (xhr.status === 200) {
                 // var results = JSON.parse(xhr.responseText);
                 console.log(xhr.responseText);
-                
+
             } else {
                 console.error(xhr.statusText);
             }
@@ -200,25 +199,27 @@ function update_forum_topiclist_results_table(results) {
         return
     }
 
-    var template = Hogan.compile(forum_topic_row_template_text, { delimiters: '<% %>' });
+    var template = Hogan.compile(forum_topic_row_template_text, {
+        delimiters: '<% %>'
+    });
     if (results instanceof Array) {
         for (var i = 0; i < results.length; i++) {
             var output = template.render(results[i]);
             $("#forum-topiclist-results").append(output)
         }
-    } else {  // should be a single element
-            var output = template.render(results);
-            $("#forum-topiclist-results").append(output)
+    } else { // should be a single element
+        var output = template.render(results);
+        $("#forum-topiclist-results").append(output)
     }
 
 
 }
 
 var forum_topic_row_template_text = [
-'    <div class="row">',
-'    	<div class="large-3 small-3 columns"> <%TopicId%> 	</div>',
-'       <div class="large-3 small-3 columns"> <%TopicName%> </div>',
-'       <div class="large-3 small-3 columns"> <%AuthorId%> 	</div>',
-'       <div class="large-3 small-3 columns"> <%CreatedAt%> </div>',
-'    </div>',
+    '    <div class="row">',
+    '       <div class="large-3 small-3 columns"> <%TopicId%>   </div>',
+    '       <div class="large-3 small-3 columns"> <%TopicName%> </div>',
+    '       <div class="large-3 small-3 columns"> <%AuthorId%>  </div>',
+    '       <div class="large-3 small-3 columns"> <%CreatedAt%> </div>',
+    '    </div>',
 ].join("\n");
